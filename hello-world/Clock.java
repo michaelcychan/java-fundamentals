c2a16a3cefef52a48451e18cc95d73334a40ad79
Michael Chan <michaelcychan@gmail.com>
2022-11-01 09:36:42 +0000

    add text styling on clock.java

--- hello-world/Clock.java
+++ hello-world/Clock.java
@@ -2,10 +2,16 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 class Clock {
+  public static final String GREEN = "\033[0;32m";   // GREEN
+
+  public static final String RESET = "\033[0m";  // Text Reset
+
+  public static final String WHITE_UNDERLINED = "\033[4;37m";  // WHITE Underline
+
   public static void main(String[] args) {
     LocalDateTime dateTime = LocalDateTime.now();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
     String formattedTime = dateTime.format(formatter);
-    System.out.printf("The current time is %s \n", formattedTime);
+    System.out.printf("The current "  + GREEN + "time" + RESET + " is " + WHITE_UNDERLINED + "%s" + RESET + ".\n", formattedTime);
   }
 }
