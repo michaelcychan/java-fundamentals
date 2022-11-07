53efbbfd68ff543ea3c28171d23a1b8cf705ec62
Michael Chan <michaelcychan@gmail.com>
2022-11-07 12:37:24 +0000

    finish getMiddle exercise

--- process-exercise/02-MiddleLetters/app/src/main/java/middleletters/GetMiddle.java
+++ process-exercise/02-MiddleLetters/app/src/main/java/middleletters/GetMiddle.java
@@ -2,6 +2,11 @@ package middleletters;
 
 public class GetMiddle {
     public static String getMiddle(String inputText){
-        return "es";
+        int strLeng = inputText.length();
+        if (strLeng % 2 == 0) {
+            return inputText.substring((strLeng/ 2) - 1, strLeng/2 + 1);
+        } else {
+            return inputText.substring(strLeng/2, strLeng/2 + 1);
+        }
     }
 }
--- process-exercise/02-MiddleLetters/app/src/test/java/middleletters/GetMiddleTest.java
+++ process-exercise/02-MiddleLetters/app/src/test/java/middleletters/GetMiddleTest.java
@@ -9,4 +9,29 @@ public class GetMiddleTest {
         String errorMessage = "The middle letters for the word \"test\" is \"es\"";
         assertEquals(errorMessage, expected, actual );
     }
+    @Test public void testWordTesting(){
+        String expected = "t";
+        String actual = GetMiddle.getMiddle("testing");
+        String errorMessage = "The middle letters for the word \"testing\" is \"t\"";
+        assertEquals(errorMessage, expected, actual );
+    }
+    @Test public void testWordMiddle(){
+        String expected = "dd";
+        String actual = GetMiddle.getMiddle("middle");
+        String errorMessage = "The middle letters for the word \"middle\" is \"dd\"";
+        assertEquals(errorMessage, expected, actual );
+    }
+    @Test public void testWordA(){
+        String expected = "A";
+        String actual = GetMiddle.getMiddle("A");
+        String errorMessage = "The middle letters for the word \"A\" is \"A\"";
+        assertEquals(errorMessage, expected, actual );
+    }
+
+    @Test public void testWordof(){
+        String expected = "of";
+        String actual = GetMiddle.getMiddle("of");
+        String errorMessage = "The middle letters for the word \"of\" is \"of\"";
+        assertEquals(errorMessage, expected, actual );
+    }
 }
